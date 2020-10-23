8648120bee64ef0769b6fd7b17b287323e40754d
Austin Tricquet <austintricquet@gmail.com>
2020-10-23 13:50:36 -0400

    Working on adding coinGecko API to get images for search

--- src/store/actions/coinSearchActions.jsx
+++ src/store/actions/coinSearchActions.jsx
@@ -9,9 +9,19 @@ import {
     try {
       console.log('BEGIN INPUT CHANGE')
       if (inputValue !== "") {
+        const responseGecko = await axios.get('https://api.coingecko.com/api/v3/coins/list')
+        
         axios.get(`https://api.coinpaprika.com/v1/search/?q=${inputValue}&c=currencies&limit=3`)
         .then(({ data }) => {
           console.log('Data Receieved!: ', data.currencies)
+          const coins = data.currencies
+          coins.forEach(coin => {
+            console.log("coin name is: ", coin.name)
+            const geckoData = responseGecko.data.find((element) => { return element.id === coin.name})
+            coin.newID = geckoData
+            console.log('coin newID ', coin.newID)
+          });
+          
           dispatch({
             type: UPDATE_SEARCH_SUCCESS,
             payload: data.currencies
