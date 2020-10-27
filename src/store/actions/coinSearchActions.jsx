75c596d6748ae8c4cd09d6160b18cefa10704185
Austin Tricquet <austintricquet@gmail.com>
2020-10-27 08:58:41 -0400

    Fixed issue where suggested coins would not be removed when removing search terms.

--- src/store/actions/coinSearchActions.jsx
+++ src/store/actions/coinSearchActions.jsx
@@ -3,32 +3,19 @@ import {
     UPDATE_SEARCH_SUCCESS,
     UPDATE_SEARCH_ERROR
   } from "./actionTypes";
-  import getState from "redux-thunk";
   import {store} from '../../index';
   
   // Signing up with Firebase
   export const handleInputChange = (inputValue) => async dispatch => {
     try {
-      console.log('BEGIN INPUT CHANGE')
       if (inputValue !== "") {
-        //const responseGecko = await axios.get('https://api.coingecko.com/api/v3/coins/list')
-        console.log('about to set state')
         const state = store.getState();
-        console.log('state: ',state);
         const responseGecko = state.onSigninReducer.coinKeys
-        console.log("about to set promise")
-        const promise1 = axios.get(`https://api.coinpaprika.com/v1/search/?q=${inputValue}&c=currencies&limit=3`)
-        console.log("about to await promise")
+        const promise1 = axios.get(`https://api.coinpaprika.com/v1/search/?q=${inputValue}&c=currencies&limit=5`)
         const coinPaprikaData = await promise1;
-        console.log("about to map response, coinpaprikadata: ", coinPaprikaData)
         const coinDataNewID = coinPaprikaData.data.currencies.map(function(response) { 
           const geckoData = responseGecko.data.find((element) => {return element.name === response.name});
           response.newID = geckoData.id
-
-          //const promise = axios.get('https://api.coingecko.com/api/v3/coins/' + response.newID)
-          //const imageData = await promise;
-          //response.image = imageData.image
-
           return {
             key: response.id,
             name: response.name,
@@ -36,8 +23,7 @@ import {
             newID: response.newID,
             image: "",
           }});
-        
-        console.log('CoinDataNewID: ', coinDataNewID);
+
         const promises = coinDataNewID.map(coin => axios.get('https://api.coingecko.com/api/v3/coins/' + coin.newID));
         const coinDataImages = await Promise.all(promises);
         const coinData = coinDataImages.map(function(response) {
@@ -58,7 +44,12 @@ import {
           type: UPDATE_SEARCH_SUCCESS,
           payload: coinData
         })
-      } 
+      } else {
+        dispatch({
+          type: UPDATE_SEARCH_SUCCESS,
+          payload: []
+        })
+      }
     } catch (err) {
       dispatch({
         type: UPDATE_SEARCH_ERROR,
