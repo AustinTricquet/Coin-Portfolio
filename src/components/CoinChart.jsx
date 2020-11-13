25f7246e9a6154f169bc908c81d1fbf98eec8f70
Austin Tricquet <austintricquet@gmail.com>
2020-11-13 12:05:35 -0500

    added the ticker symbol to the chart title.

--- src/components/CoinChart.jsx
+++ src/components/CoinChart.jsx
@@ -75,8 +75,6 @@ const Canvas = styled.canvas`
 `;
 
 const CoinChart = ({selectedCoin}) => {
-    //const ctx = document.getElementById('coinChart').getContext('2d');
-    
 
     useEffect(() => {
         
@@ -162,122 +160,22 @@ const CoinChart = ({selectedCoin}) => {
                }
             });
 
-
-
-
-
-
-
-
-
-            /* Chart.defaults.LineWithLine = Chart.defaults.line;
-            Chart.controllers.LineWithLine = Chart.controllers.line.extend({
-                draw: function(ease) {
-                    coinChart.controllers.line.prototype.draw.call(this, ease);
-
-                    if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
-                        var activePoint = this.chart.tooltip._active[0],
-                            ctx = this.chart.ctx,
-                            x = activePoint.tooltipPosition().x,
-                            topY = this.chart.legend.bottom,
-                            bottomY = this.chart.chartArea.bottom;
-
-                        //draw line
-                        ctx.save();
-                        ctx.beginPath();
-                        ctx.moveTo(x, topY);
-                        ctx.lineTo(x, bottomY);
-                        ctx.lineWidth = 2;
-                        ctx.strokeStyle = '#07C';
-                        ctx.stroke();
-                        ctx.restore();
-                    }
-                }
-            });
-            
-            const chartPrices = selectedCoin[0].chartPrices;
-            const chartDates = selectedCoin[0].chartDates;
-            //console.log("Date example: ", Date(chartDates[5]),);
-            //console.log("USE EFFECT RAN!!!")
-            
-            const config = {
-                type: 'LineWithLine',
-                data: {
-                    labels: chartDates,
-                    datasets: [{
-                        legend: {
-                            display: false
-                        },
-                        label: 'Bitcoin',
-                        pointRadius: 1,
-                        data: chartPrices,
-                        backgroundColor: [
-                          
-                            //'rgba(255, 99, 132, 0.2)',
-                            //'rgba(54, 162, 235, 0.2)',
-                            //'rgba(255, 206, 86, 0.2)',
-                            //'rgba(75, 192, 192, 0.2)',
-                            //'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.5)'
-                        ],
-                        borderColor: [
-                            
-                            //'rgba(255, 99, 132, 1)',
-                            //'rgba(54, 162, 235, 1)',
-                            //'rgba(255, 206, 86, 1)',
-                            //'rgba(75, 192, 192, 1)',
-                            //'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, .8)'
-                        ],
-                        borderWidth: 3,
-                        pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)',
-                        pointHoverRadius: 4,
-                      
-                    }]
-                },
-                options: {
-                    legend: {
-                        display: false
-                    },
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: false
-                            }
-                        }]
-                    },
-                    tooltips: {
-                        intersect: false
-                    },
-                    responsive: false
-                    
-
-                }
-            }
-            const coinChart = new Chart(ctx, config) */
-            
-
             return function cleanup() {
                 try {
                 console.log("DESTROY CHART")
                 chart.destroy();
                 } catch {}
-            }
-            
-    
+            }  
             
         } catch {    
         }
-
-        
-
     
     })
     
     return (
         <Div>
             <Header>
-                <h3>USD/BTC</h3>
+                <h3>USD/{selectedCoin[0].symbol}</h3>
                 <ChartButtonGroup>
                     <button>24h</button>
                     <button>7d</button>
